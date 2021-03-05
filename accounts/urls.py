df4abd69778d6e041cd1204310de3b8416e1f348
unknown <amitgautam1994@gmail.com>
2021-03-05 17:45:36 +0530

    Create, add and delete order

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('customer/<str:cust_id>', views.customer, name='customer'),
+    path('create_order', views.createOrder, name='create_order'),
+    path('update_order/<str:order_id>', views.updateOrder, name='update_order'),
+    path('delete_item/<str:order_id>', views.deleteOrder, name='delete_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 # Create your views here
 
 
@@ -26,3 +27,33 @@ def customer(request, cust_id):
     total_orders = orders.count()
     customer_details = {'customer':customer, 'orders':orders, 'total_orders':total_orders}
     return render(request, 'accounts/customer.html', customer_details)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, order_id):
+    order = Order.objects.get(id=order_id)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, order_id):
+    order = Order.objects.get(id=order_id)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
